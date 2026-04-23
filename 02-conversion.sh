a75ff6e3dad1d1f7e31520ab845d1129e42f32eb
shaikalthaf1984 <shaikalthaf1984@gmail.com>
2026-04-23 11:07:56 +0530

    02-conversion.sh

---
+++ 02-conversion.sh
@@ -0,0 +1,6 @@
+#!/bins/bash
+
+echo "Ramesh:: Hi Suresh, How are you"
+echo "suresh:: Hellow Ramesh, I am fine, how are you?"
+echo "Ramesh:: what you learning recently?"
+echo "Suresh:: I am learning shell scripting Ramesh"

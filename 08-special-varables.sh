961d29083958b17f4ce38c0aa458ea6a2659b5d8
shaikalthaf1984 <shaikalthaf1984@gmail.com>
2026-04-23 13:27:05 +0530

    05-varables.sh

--- 08-special-varables.sh
+++ 08-special-varables.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "All variables passwd: $@"
-#echo "All varables passed: $*"
+echo "All variables passwd to the script: $@"
+echo "All varables passed to the script: $*"
+
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "who is running this: $user"
+echo "home directory: $Home"
+echo "pid of this script" $$
+echo "pid of last command in background is:$!"

edb25a2968b93224cb2f5dc0fc1641025c274bde
shaikalthaf1984 <shaikalthaf1984@gmail.com>
2026-04-23 11:46:55 +0530

    04-varables.sh

---
+++ 04-varables.sh
@@ -0,0 +1,8 @@
+
+person1=$1
+person2=$2
+
+echo "$person1:: welcome to alaska $person2"
+echo "$person2:: thank you $person1, finally not on zoon webiner"
+echo "$person1:: so do we have a deal"
+echo "$person2:: yes, if you make ukraine joins russia"

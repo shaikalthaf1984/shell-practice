35f6163f9771c031b0ceddc9eb175a1fe972f9bc
shaikalthaf1984 <shaikalthaf1984@gmail.com>
2026-04-23 11:21:43 +0530

    variable

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=john
+person2=rahem
+
+echo "$person1:: Hi $person2, how are you?
+echo "$person2:: Hello $person1, i am fine how are you?
+echo "$person1:: what are you learning" $person2
+echo "$person2:: i am learning devsecops $person1
+
